be9342204f68c1665db506daf0d7fa0413dee819
Jan Wolf <jan.wolf@ssrd.io>
2024-11-12 22:57:55 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {createServer} from 'http';
 import express from 'express';
+import cors from 'cors';
 import {connectMongoDb} from './database/connection';
 
 import swaggerUi from 'swagger-ui-express';
@@ -13,6 +14,7 @@ dotenv.config();
 
 const app = express()
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
