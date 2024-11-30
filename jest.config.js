876daf0b1876a99662171ff5c6fb429811c08548
Jan Wolf <jan.wolf@ssrd.io>
2024-11-30 13:46:12 +0100

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
